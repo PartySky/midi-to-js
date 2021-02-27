22c72d3bb3836d405345e07b0f9b6d94444ec273
Yuri Partitski <PartySky@users.noreply.github.com>
2021-02-27 18:20:33 +0200

    Implemented getting notes by barFirstSegmentDuration

--- src/Classes/main.ts
+++ src/Classes/main.ts
@@ -37,13 +37,37 @@ export class Main {
             const notes = track.notes;
             notes.forEach((note: Note) => {
                 console.log(note);
-                note.pitch = 'G';
-                note.velocity = 0.75;
+                // note.pitch = 'G';
 
                 //note.midi, note.time, note.duration, note.name
             });
 
-            notes[1].ticks = this.measuresToTicks(2.5, midi.header);
+            const eighthBarNotes: Note[] = this.getNDurationBarNotes(track.notes, midi.header, 1/8);
+            const quarterBarNotes: Note[] = this.getNDurationBarNotes(track.notes, midi.header, 1/4);
+            const halfBarNotes: Note[] = this.getNDurationBarNotes(track.notes, midi.header, 1/2);
+            const wholeBarNotes: Note[] = this.getNDurationBarNotes(track.notes, midi.header, 1/1);
+
+            notes.forEach(note => {
+               note.velocity = 0.01; 
+            });
+
+            // sixteenthBarNotes.forEach(note => {
+            //     note.velocity = 0.25;
+            // });
+            eighthBarNotes.forEach(note => {
+                note.velocity = 0.25;
+            });
+            quarterBarNotes.forEach(note => {
+                note.velocity = 0.5;
+            });
+            halfBarNotes.forEach(note => {
+               note.velocity = 0.75; 
+            });
+            wholeBarNotes.forEach(note => {
+                note.velocity = 1;
+            });
+            
+            // notes[1].ticks = this.measuresToTicks(2.5, midi.header);
 
             //the control changes are an object
             //the keys are the CC number
@@ -73,5 +97,29 @@ export class Main {
     measuresToTicks(bars: number, header: Header): number {
         let ticks = bars * header.ppq * 4;
         return ticks;
+    }
+
+    private getNDurationBarNotes(notes: Note[], header: Header, barFirstSegmentDuration: number, barSize: number = 1): Note[] {
+        const result: Note[] = [];
+        const delta = 1/16;
+        const coef = barFirstSegmentDuration;
+        
+        notes.forEach(note => {
+            const value = note.bars;
+            
+            /**
+             * There is no neceserelly include the first and last notes
+             * because they will be included in a wholeBarNotes array
+             */
+            for(let coefSummed = 0; coefSummed <= barSize; coefSummed = coefSummed + coef) {
+                if(
+                    ((value % 1 >= coefSummed) && (value % 1 < coefSummed + delta)) ||
+                    ((value % 1 >= coefSummed - delta) && (value % 1 < coefSummed))
+                ) {
+                    result.push(note);
+                };
+            }
+        });
+        return result;
     }
 }
